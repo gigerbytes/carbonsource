41c0c4ddc2a8525aa212b57c2ccb6ecd036b5ca6
LMCMLJ <mjcalvey2@gmail.com>
2016-11-30 01:37:48 +0000

    Scaffolding in place for search controller, views and subject model

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,10 @@
+class SearchController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def list
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'search/new'
+
+  get 'search/show'
+
+  get 'search/list'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
